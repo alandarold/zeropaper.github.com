25913f8c3ba27472672fffbabdde6a9cdaa03a05
Valentin Vago <zeropaper@irata.ch>
2011-07-24 13:42:28 +0200

    + ‘.buildpath’ + ‘.project’ + ‘.sass-cache/1f5052f69f2eec4de705dfc4cd4edaf6211677ea/_utilities.scssc’ + ‘.sass-cache/2400346d8becacbf427738cfbf3e60ad883c1c99/_reset.scssc’ + ‘.sass-cache/3e8f0775fcb49cea7b4b1e5db022eeecfe7d295e/_grid.sassc’ + ‘.sass-cache/3e8f0775fcb49cea7b4b1e5db022eeecfe7d295e/_text.sassc’ + ‘.sass-cache/817e8b85b749e6bb0b85790c4135d957b5b0af2a/_sticky-footer.scssc’ + ‘.sass-cache/a7e6227fdc6dd8443434ba3380b56fc27bcdd68a/_base.scssc’ + ‘.sass-cache/a7e6227fdc6dd8443434ba3380b56fc27bcdd68a/all.scssc’ + ‘.sass-cache/a7e6227fdc6dd8443434ba3380b56fc27bcdd68a/grid.scssc’ + ‘.sass-cache/a7e6227fdc6dd8443434ba3380b56fc27bcdd68a/screen.scssc’ + ‘.sass-cache/a7e6227fdc6dd8443434ba3380b56fc27bcdd68a/text.scssc’ + ‘.settings/.jsdtscope’ + ‘.settings/org.eclipse.php.core.prefs’ + ‘.settings/org.eclipse.wst.jsdt.ui.superType.container’ + ‘.settings/org.eclipse.wst.jsdt.ui.superType.name’ + ‘compass_app_log.txt’ + ‘config.rb’ + ‘css/all.css’ + ‘css/grid.css’ ...

---
+++ config.rb
@@ -0,0 +1,22 @@
+# Require any additional compass plugins here.
+
+
+project_type = :stand_alone
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "images"
+fonts_dir = "fonts"
+javascripts_dir = "js"
+
+output_style = :compressed
+output_style = :nested
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+relative_assets = true
+
+# http://groups.google.com/group/compass-users/browse_thread/thread/417429db92a92305
+sass_options = {:cache_location => "/tmp/sass-cache", :debug_info => true}
---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+
+
+(function($, win, undefined){
+
+})(jQuery, window);
+
